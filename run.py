cc96bc08cbf2fc104da64883fe45e0bcb0f804b5
Nils Wireklint <nils@meroton.com>
2025-06-26 13:39:02 +0200

    Tidy up: update to working python runfiles

--- run.py
+++ run.py
@@ -6,12 +6,12 @@ import subprocess
 import sys
 from typing import List
 
-from rules_python.python.runfiles import \
-    runfiles  # Bazel's runfiles library, to find runtime dependencies.
+# Bazel's runfiles library, to find runtime dependencies.
+from python.runfiles import Runfiles
 
 
 def main(args: List[str]):
-    r = runfiles.Create()
+    r = Runfiles.Create()
     # Workspace-relative path.
     program = r.Rlocation("example/Program")
     subprocess.run([program, "from", "python"]),
